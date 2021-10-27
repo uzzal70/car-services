b50e4842e35f27171e267a1ab6b98a5507f415a1
uzzal70 <uzzalcse70@gmail.com>
2021-10-27 23:52:28 +0600

    addall

---
+++ src/Pages/Booking/Booking/Booking.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+
+const Booking = () =>
+{
+    const { serviceId } = useParams();
+    const [service, setService] = useState({});
+    useEffect(() =>
+    {
+        fetch(`http://localhost:5000/services/${serviceId}`)
+            .then(res => res.json())
+            .then(data => setService(data));
+    }, []);
+
+    return (
+        <div>
+            <h2>Details of: {service?.name}</h2>
+            <h2>this is booking: {serviceId}</h2>
+            <h1>hi</h1>
+        </div>
+    );
+};
+
+export default Booking;
